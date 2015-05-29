9f7a2493de7e4cc2d1ac48f03d56dbf7c48b5072
J C Lawrence <jcl@cydesign.com>
2015-05-29 16:58:57 -0700

    Prep for PyPi

--- setup.py
+++ setup.py
@@ -3,26 +3,29 @@ from setuptools import setup, find_packages
 __version__ = "unknown"
 
 import pyver
-__version__, __version_info__ = pyver.get_version (pkg = "jsondict")
+__version__, __version_info__ = pyver.get_version (
+    pkg = "jsondict",
+    public = True)
 
-setup (name = "jsondict",
-  version = __version__,
-  description = "A dict optionally backed by an auto-updating JSON file.",
-  long_description = file ("README.rst").read (),
-  classifiers = [],
-  keywords = "",
-  author = "J C Lawrence",
-  author_email = "claw@kanga.nu",
-  url = "http://kanga.nu/~claw/",
-  license = "GPL v3",
-  packages = find_packages (exclude = ["tests",]),
-  package_data = {
-  },
-  zip_safe = True,
-  install_requires = [line.strip ()
-                      for line in file ("requirements.txt").readlines ()],
-  entry_points = {
-      "console_scripts": [
-      ],
-  },
+setup (
+    name = "jsondict.py",
+    version = __version__,
+    description = "A dict optionally backed by an auto-updating JSON file.",
+    long_description = file ("README.rst").read (),
+    classifiers = [],
+    keywords = "",
+    author = "J C Lawrence",
+    author_email = "claw@kanga.nu",
+    url = "http://kanga.nu/~claw/",
+    license = "GPL v3",
+    packages = find_packages (exclude = ["tests",]),
+    package_data = {
+    },
+    zip_safe = True,
+    install_requires = [line.strip ()
+                        for line in file ("requirements.txt").readlines ()],
+    entry_points = {
+        "console_scripts": [
+        ],
+    },
 )
